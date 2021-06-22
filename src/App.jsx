f723e71e4a7df498cbe951f1405394e9893f762c
astomak <harshakaastomak@gmail.com>
2021-06-22 23:30:51 +0530

    Created react-template with tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="bg-red-500 text-blue-500">
+      <span className="italic text-4xl">HELLO</span>
+    </div>
+  )
+}
+
+export default App
